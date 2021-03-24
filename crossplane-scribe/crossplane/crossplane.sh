ecc62e5d06d15408a1d127af44de5cbeb6f25800
Krish Chowdhary <krish@redhat.com>
2021-03-24 17:26:44 -0400

    crossplane-scribe: crossplane version update, switch to providerConfig

---
+++ crossplane-scribe/crossplane/crossplane.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source variables.env
+
+kubectl create namespace crossplane-system
+
+helm repo add crossplane-stable https://charts.crossplane.io/stable
+helm repo update
+
+helm install crossplane --namespace crossplane-system crossplane-stable/crossplane --set securityContextCrossplane.runAsUser=null --set securityContextCrossplane.runAsGroup=null --version v1.1.0
