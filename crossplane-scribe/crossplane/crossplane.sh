78121093a6247b7f73ece3cd6ed4bb3054c6b64d
Ryan Cook <rcook@redhat.com>
2021-05-12 17:51:27 -0400

    ship for demo

--- crossplane-scribe/crossplane/crossplane.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace crossplane-system
-
-helm repo add crossplane-stable https://charts.crossplane.io/stable
-helm repo update
-
-helm install crossplane --namespace crossplane-system crossplane-stable/crossplane --set securityContextCrossplane.runAsUser=null --set securityContextCrossplane.runAsGroup=null --version v1.0.0
